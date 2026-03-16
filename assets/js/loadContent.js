e0cbd24bb14b2e87ed8837301a4034500d9fec31
Madondnh <madondnh@gmail.com>
2026-03-16 22:30:59 +0200

    ADDED ADELAY SETUP TO ALLOW FOR ANIMATION PLAY

--- assets/js/loadContent.js
+++ assets/js/loadContent.js
@@ -1,25 +1,32 @@
 // loadContent.js - dynamically inject header and main content
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", async () => {
 
-    console.log("DOM fully loaded and parsed");
-    
-    const loadFragment = async (url, containerId) => {
-        try {
-            const res = await fetch(url);
-            if (!res.ok) throw new Error(`Failed to load ${url}: ${res.status}`);
-            const html = await res.text();
+ // SET UP A BIT OF A DELAY TO SIMULATE LOADING TIME
+  await new Promise((resolve) => setTimeout(resolve, 1000));
 
-            document.getElementById(containerId).outerHTML = html;
-        
-        } catch (err) {
-            console.error(err);
-        }
-    };
+  console.log("DOM fully loaded and parsed");
 
-    loadFragment('header.html', 'nav-container');
-    loadFragment('main_content.html', 'main-container');
+  const loadFragment = async (url, containerId) => {
+    try {
+      const res = await fetch(url);
+      if (!res.ok) throw new Error(`Failed to load ${url}: ${res.status}`);
+      const html = await res.text();
 
-    loadUtilFunction();
-    initialiseMainPageAsync();
+      document.getElementById(containerId).outerHTML = html;
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  await loadFragment("header.html", "nav-container");
+  await loadFragment("main_content.html", "main-container");
+
+  loadUtilFunction();
+  await initialiseMainPageAsync();
+
+  // Sidebar.
+  var $loader = $("#loading-overlay");
+  $loader.removeClass("is-preload");
+  $loader.addClass("notloading");
 });
